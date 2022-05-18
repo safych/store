da01fb2e3d54571527683d10b5251b5736bcbeb8
safych <v7lad@ukr.net>
2022-05-18 12:20:08 +0300

    Migration code improved

--- db/migrate/20220510161532_create_products.rb
+++ db/migrate/20220510161532_create_products.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 class CreateProducts < ActiveRecord::Migration[6.1]
   def change
     create_table :products do |t|
       t.string :name, null: false
-      t.text :description 
+      t.text :description
       t.string :size, null: false
       t.integer :items_left, null: false
       t.integer :price, null: false
