606db5f0d4ae77df597a3d60814e43d6fc9936b9
safych <v7lad@ukr.net>
2022-06-25 13:13:00 +0300

    Secrets ingnored

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,16 @@
-class UserMailer < ApplicationMailer
-  default from: 'notifications@example.com'
+# frozen_string_literal: true
 
+# require 'mailgun-ruby'
+
+class UserMailer < ApplicationMailer
   def reset_password
     @user = params[:user]
     mail(to: @user.email, subject: 'Код для відновлення паролю')
+    # mg_client = Mailgun::Client.new 'your-api-key'
+    # message_params = { from: 'bob@sending_domain.com',
+    #                    to: @user.email,
+    #                    subject: 'Відновлення паролю акаунту',
+    #                    text: "Ваш код для відновлення паролю #{@user.recovery_code}" }
+    # mg_client.send_message 'sending_domain.com', message_params
   end
 end
